d5370873018930a2680c656f2bcee3b61755911f
John Foley <jfoley@cs.umass.edu>
2017-10-09 21:08:28 -0400

    query likelihood is much stronger than #lib operator

---
+++ src/main/kotlin/edu/umass/cics/ciir/lim/LeastInformationOps.kt
@@ -0,0 +1,41 @@
+package edu.umass.cics.ciir.lim
+
+import org.lemurproject.galago.core.retrieval.RequiredStatistics
+import org.lemurproject.galago.core.retrieval.iterator.CountIterator
+import org.lemurproject.galago.core.retrieval.iterator.LengthsIterator
+import org.lemurproject.galago.core.retrieval.iterator.ScoringFunctionIterator
+import org.lemurproject.galago.core.retrieval.processing.ScoringContext
+import org.lemurproject.galago.core.retrieval.query.NodeParameters
+
+@RequiredStatistics(statistics = arrayOf("collectionLength", "nodeFrequency", "documentCount", "nodeDocumentCount"))
+class LeastInformationBinary(np: NodeParameters, lengths: LengthsIterator, counts: CountIterator) : ScoringFunctionIterator(np, lengths, counts) {
+
+    // Total term occurrences in collection...
+    val collectionLength = np.getLong("collectionLength")
+    // Total term occurrences.
+    val collectionFrequency = np.getLong("nodeFrequency")
+    // Total docs in collection
+    val N = np.getLong("documentCount")
+    // n_i
+    val df = np.getLong("nodeDocumentCount")
+
+    val gtC = g(df.toDouble() / N.toDouble())
+
+    // This is the core function of LIB: http://www.wolframalpha.com/input/?i=x*(1+-+ln(x))+from+x+%3D+0+to+x+%3D+1
+    fun g(p: Double) = p * (1 - Math.log(p))
+
+    override fun score(c: ScoringContext?): Double {
+        val count = this.countIterator.count(c)
+        if (count > 0) {
+            return 1.0 - gtC;
+        } else {
+            return -gtC;
+        }
+    }
+
+    override fun hasMatch(context: ScoringContext?): Boolean {
+        // hack to verify that negative scores are not useful.
+        //return true
+        return super.hasMatch(context)
+    }
+}
--- src/main/kotlin/edu/umass/cics/ciir/lim/Main.kt
+++ src/main/kotlin/edu/umass/cics/ciir/lim/Main.kt
@@ -15,10 +15,11 @@ typealias GExpr = org.lemurproject.galago.core.retrieval.query.Node
 object DataPaths {
     val host: String = InetAddress.getLocalHost().hostName
     fun notImpl(): Nothing = throw RuntimeException("notImpl")
-    fun getRobustIndex(): LocalRetrieval = LocalRetrieval(when(host) {
+    fun getRobustIndex(): LocalRetrieval = getRobustIndex(Parameters.create())
+    fun getRobustIndex(p: Parameters): LocalRetrieval = LocalRetrieval(when(host) {
         "gob" -> "/media/jfoley/flash/robust04.galago"
         else -> notImpl()
-    })
+    }, p)
     fun getQueryDir(): File = File(when(host) {
         "gob" -> "/home/jfoley/code/queries/robust04/"
         else -> notImpl()
@@ -39,10 +40,12 @@ object DataPaths {
     fun getQueryJudgments(): QuerySetJudgments = QuerySetJudgments(getQueryJudgmentsFile().absolutePath, false, false);
 }
 
+
 fun GExpr.push(what: GExpr): GExpr {
     this.addChild(what)
     return this
 }
+
 
 class NamedMeasures {
     val measures = HashMap<String, TDoubleArrayList>()
@@ -52,8 +55,17 @@ class NamedMeasures {
     fun means(): TreeMap<String, Double> = measures.mapValuesTo(TreeMap()) { (_,arr) -> arr.sum() / arr.size() }
 }
 
+fun LocalRetrieval.exec(expr: GExpr): QueryResults = QueryResults(this.transformAndExecuteQuery(expr).scoredDocuments)
+
 fun main(args: Array<String>) {
-    DataPaths.getRobustIndex().use { ret ->
+
+    val operators = Parameters.create()
+    operators.set("lib", LeastInformationBinary::class.java.canonicalName)
+
+    val indexP = Parameters.create()
+    indexP.put("operators", operators)
+
+    DataPaths.getRobustIndex(indexP).use { ret ->
         val tokenizer = ret.tokenizer!!
 
         val tqs = DataPaths.getTitleQueries()
@@ -66,17 +78,18 @@ fun main(args: Array<String>) {
             val qterms = tokenizer.tokenize(qtext).terms.filterNotNull()
             val truth = qrels.get(qid)!!
 
+            val libExpr = GExpr("wsum")
             val qlExpr = GExpr("combine")
             qterms.forEach {
                 qlExpr.push(GExpr.Text(it))
+                libExpr.push(GExpr("lib").push(GExpr.Text(it)))
             }
 
-            val results = ret.transformAndExecuteQuery(qlExpr)!!
-            val score = AP.evaluate(QueryResults(results.scoredDocuments), truth)
-            measures.push("ql-ap", score)
+            measures.push("ql-ap", AP.evaluate(ret.exec(qlExpr), truth))
+            measures.push("lib-ap", AP.evaluate(ret.exec(libExpr), truth))
             println(measures.means())
         }
-        println(measures.means())
+        println(Parameters.wrap(measures.means()))
 
         //ret.index.getIndexPart()
         //println(index..toPrettyString())
